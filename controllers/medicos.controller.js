f6c3ae2270b7c9bde43433d6409e6595ce68676c
Nahuel Bentos <nahuelbentosgnocchi@gmail.com>
2020-08-27 00:29:38 -0300

    fin sección 17

--- controllers/medicos.controller.js
+++ controllers/medicos.controller.js
@@ -1,109 +1,128 @@
 const { response } = require('express');
 
 const Medico = require('../models/medico.model');
-const getMedicos = async(req, res = response) => {
-    try {
-        const medicos = await Medico.find().populate('usuario', 'nombre img').populate('hospital', 'nombre img');
+const getMedicos = async (req, res = response) => {
+  try {
+    const medicos = await Medico.find().populate('usuario', 'nombre img').populate('hospital', 'nombre img');
 
-        res.json({
-            ok: true,
-            medicos,
-        });
-    } catch (error) {
-        console.log(error);
-        res.status(500).json({
-            ok: false,
-            msg: 'Error inesperado',
-        });
-    }
-};
-
-const crearMedico = async(req, res = response) => {
-    const uid = req.uid;
-    const medico = new Medico({
-        usuario: uid,
-        ...req.body,
+    res.json({
+      ok: true,
+      medicos,
     });
-
-    try {
-        const medicoDB = await medico.save();
-        res.json({
-            ok: true,
-            medico: medicoDB,
-        });
-    } catch (error) {
-        console.log(error);
-        res.status(500).json({
-            ok: false,
-            msg: 'Error inesperado',
-        });
-    }
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({
+      ok: false,
+      msg: 'Error inesperado',
+    });
+  }
 };
 
-const actualizarMedico = async(req, res = response) => {
-    try {
-        const id = req.params.id;
-        const uid = req.uid;
+const getMedicoById = async (req, res = response) => {
+  const id = req.params.id;
+  try {
+    const medico = await Medico.findById(id).populate('usuario', 'nombre img').populate('hospital', 'nombre img');
 
-        const medico = await Medico.findById(id);
-        if (!medico) {
-            return res.status(404).json({
-                ok: true,
-                msg: 'Médico no encontrado.',
-            });
-        }
-
-        const cambiosMedico = {
-            ...req.body,
-            usuario: uid,
-        };
-
-        const medicoActualizado = await Medico.findByIdAndUpdate(id, cambiosMedico, { new: true });
-        medico.nombre = req.body.nombre;
-
-        res.json({
-            ok: true,
-            medico: medicoActualizado,
-        });
-    } catch (error) {
-        console.log(error);
-        res.status(500).json({
-            ok: false,
-            msg: 'Error inesperado',
-        });
-    }
+    res.json({
+      ok: true,
+      medico,
+    });
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({
+      ok: false,
+      msg: 'Error inesperado',
+    });
+  }
 };
 
-const borrarMedico = async(req, res = response) => {
-    try {
-        const id = req.params.id;
+const crearMedico = async (req, res = response) => {
+  const uid = req.uid;
+  const medico = new Medico({
+    usuario: uid,
+    ...req.body,
+  });
 
-        const medico = await Medico.findById(id);
-        if (!medico) {
-            return res.status(404).json({
-                ok: true,
-                msg: 'Médico no encontrado.',
-            });
-        }
+  try {
+    const medicoDB = await medico.save();
+    res.json({
+      ok: true,
+      medico: medicoDB,
+    });
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({
+      ok: false,
+      msg: 'Error inesperado',
+    });
+  }
+};
 
-        await Medico.findByIdAndDelete(id);
+const actualizarMedico = async (req, res = response) => {
+  try {
+    const id = req.params.id;
+    const uid = req.uid;
 
-        res.json({
-            ok: true,
-            msg: 'Médico eliminado',
-        });
-    } catch (error) {
-        console.log(error);
-        res.status(500).json({
-            ok: false,
-            msg: 'Error inesperado',
-        });
+    const medico = await Medico.findById(id);
+    if (!medico) {
+      return res.status(404).json({
+        ok: true,
+        msg: 'Médico no encontrado.',
+      });
     }
+
+    const cambiosMedico = {
+      ...req.body,
+      usuario: uid,
+    };
+
+    const medicoActualizado = await Medico.findByIdAndUpdate(id, cambiosMedico, { new: true });
+    medico.nombre = req.body.nombre;
+
+    res.json({
+      ok: true,
+      medico: medicoActualizado,
+    });
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({
+      ok: false,
+      msg: 'Error inesperado',
+    });
+  }
+};
+
+const borrarMedico = async (req, res = response) => {
+  try {
+    const id = req.params.id;
+
+    const medico = await Medico.findById(id);
+    if (!medico) {
+      return res.status(404).json({
+        ok: true,
+        msg: 'Médico no encontrado.',
+      });
+    }
+
+    await Medico.findByIdAndDelete(id);
+
+    res.json({
+      ok: true,
+      msg: 'Médico eliminado',
+    });
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({
+      ok: false,
+      msg: 'Error inesperado',
+    });
+  }
 };
 
 module.exports = {
-    getMedicos,
-    crearMedico,
-    actualizarMedico,
-    borrarMedico,
+  getMedicos,
+  getMedicoById,
+  crearMedico,
+  actualizarMedico,
+  borrarMedico,
 };
--- routes/medicos.js
+++ routes/medicos.js
@@ -6,31 +6,35 @@ const { Router } = require('express');
 const { check } = require('express-validator');
 
 const { validarJWT } = require('../middlewares/validar-jwt');
-const { getMedicos, crearMedico, actualizarMedico, borrarMedico } = require('../controllers/medicos.controller');
+const { getMedicos, getMedicoById, crearMedico, actualizarMedico, borrarMedico } = require('../controllers/medicos.controller');
 const { validarCampos } = require('../middlewares/validar-campos');
 
 const router = Router();
 
-router.get('/', [], getMedicos);
+router.get('/', [validarJWT], getMedicos);
+
+router.get('/:id', [validarJWT], getMedicoById);
 
 router.post(
-    '/', [
-        validarJWT,
-        check('nombre', 'El nombre del medico es necesario').not().isEmpty(),
-        check('hospital', 'El hospital id debe de ser valido').isMongoId(),
-        validarCampos,
-    ],
-    crearMedico
+  '/',
+  [
+    validarJWT,
+    check('nombre', 'El nombre del medico es necesario').not().isEmpty(),
+    check('hospital', 'El hospital id debe de ser valido').isMongoId(),
+    validarCampos,
+  ],
+  crearMedico
 );
 
 router.put(
-    '/:id', [
-        validarJWT,
-        check('nombre', 'El nombre del medico es necesario').not().isEmpty(),
-        check('hospital', 'El hospital id debe de ser valido').isMongoId(),
-        validarCampos,
-    ],
-    actualizarMedico
+  '/:id',
+  [
+    validarJWT,
+    check('nombre', 'El nombre del medico es necesario').not().isEmpty(),
+    check('hospital', 'El hospital id debe de ser valido').isMongoId(),
+    validarCampos,
+  ],
+  actualizarMedico
 );
 
 router.delete('/:id', [validarJWT], borrarMedico);
