c9f164caa2f3d33b0142fd3a1793133557478a00
Nahuel Bentos <nahuel.bentos@fing.edu.uy>
2020-08-27 16:34:56 -0300

    SPA fix

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ require('dotenv').config();
 
 const express = require('express');
 const cors = require('cors');
+const path = require('path');
 
 const { dbConnection } = require('./database/config');
 
@@ -27,6 +28,11 @@ app.use('/api/medicos', require('./routes/medicos'));
 app.use('/api/hospitales', require('./routes/hospitales'));
 app.use('/api/busqueda', require('./routes/busqueda'));
 app.use('/api/upload', require('./routes/uploads'));
+
+//Configuración para que funcione el refresh de la carpeta 'public'es decir, la app angular.
+app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'public/index.html'));
+});
 
 app.listen(process.env.PORT, () => {
     console.log(`Servidor corriendo en puerto   ${process.env.PORT}`);
